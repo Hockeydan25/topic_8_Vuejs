7cc852f20d03468572bbc71c13f2879323182bd9
Dan Smestad <hockeydan25@gmail.com>
2021-11-09 08:16:08 -0600

    quiz result testing for winter and temp

--- quizApi.js
+++ quizApi.js
@@ -1,2 +1,7 @@
 let trees = ['oak', 'palm', 'birch']
 trees.forEach( tree => console.log(tree) )// is this a forEach method or function.
+
+    
+let temp = 30
+let winter = temp < 32 ? true: false
+console.log(temp)  //temp is 30 and winter is true because it is less then 32.
